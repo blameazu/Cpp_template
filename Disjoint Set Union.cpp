8d081416ada19250f5d8d7faad498255ffac195b
Blameazu <pig96922@gmail.com>
2025-11-21 15:11:12 +0800

    DSU updated

--- Disjoint Set Union.cpp
+++ Disjoint Set Union.cpp
@@ -10,12 +10,15 @@ struct DSU{
                 bool ans = dsu.same(a, b); // True: a & b is connected
             Components numbers
                 int re = dsu.com;
+            Get x's Component's size
+                int re = dsu.get_sz(x);
     */
 	int com;
 	vector<int> p;
 	DSU(int n) : com(n), p(n+1, -1) {}
 	int fp(int id) {return p[id] < 0 ? id : p[id] = fp(p[id]);}
 	bool same(int a, int b) {return fp(a) == fp(b);}
+    int get_sz(int id) {return -p[fp(id)];}
 	void upd(int a, int b) {
 		int ar = fp(a), br = fp(b);
 		if(ar != br) {
